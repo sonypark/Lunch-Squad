4fe97f89b82b318e6373a8b48c3188fcb89a95b3
Sony <sonypark0204@gmail.com>
2019-10-18 19:23:38 +0900

    유저 access token 검증 로직 구현

--- api/users.js
+++ api/users.js
@@ -4,6 +4,8 @@ const uuid = require('uuid');
 const AWS = require('aws-sdk');
 AWS.config.setPromisesDependency(require('bluebird'));
 
+const decodeJwt = require('../utils/decode-verify-jwt');
+
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 // Cognito Post Confirmation Event를 받은 후 DynamoDB에 User 정보 저장
@@ -11,7 +13,7 @@ module.exports.registerUserToDynamoDB = async (event, context, callback) => {
   const user_info = {
     TableName: process.env.USER_TABLE,
     Item: {
-      userId: uuid.v4(),
+      userId: event.request.userAttributes.sub,
       email: event.request.userAttributes.email,
       nickname: event.request.userAttributes.nickname
     }
@@ -27,5 +29,20 @@ module.exports.registerUserToDynamoDB = async (event, context, callback) => {
   } catch (error) {
     // Send error message
     context.fail(err);
+  }
+};
+
+module.exports.verifyJwt = async (event, context, callback) => {
+  try {
+    const result = await decodeJwt(event.headers);
+    if (result) {
+      console.log(`Verification is ${result.isValid}`);
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(result)
+      });
+    }
+  } catch (error) {
+    console.error(error);
   }
 };
